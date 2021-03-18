f9c15e3a3e601c21983d9c8378998992ba25de4b
Tooopik <adrian.wojcik00@gmail.com>
2021-03-18 12:20:41 +0100

    S10L57

---
+++ S10/S10L56.py
@@ -0,0 +1,31 @@
+import time
+
+
+def function_performace(func, *arg, how_many_times=1):
+    sum = 0
+    for i in range(0, how_many_times):
+        start = time.perf_counter()
+        func(*arg)
+        stop = time.perf_counter()
+        sum = sum + (stop - start)
+    return sum
+
+
+def is_element_in(number, gen):
+    if number in gen:
+        return "Yes"
+
+    else:
+        return "No"
+
+
+setContainer = {i for i in range(1000)}
+listContainer = [i for i in range(1000)]
+
+print("SET :", is_element_in(1010, setContainer))
+print("SET TIME :", function_performace(
+    is_element_in, 1010, setContainer, how_many_times=500000))
+
+print("LIST :", is_element_in(1010, listContainer))
+print("LIST TIME :", function_performace(
+    is_element_in, 1010, listContainer, how_many_times=500000))
---
+++ S10/S10L57.py
@@ -0,0 +1,5 @@
+def count(*arg):
+    return sum(arg)
+
+
+print(count(2, 4, 1, 2, 4, 5, 10))
